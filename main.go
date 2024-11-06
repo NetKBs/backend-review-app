7d89e8f6bde78b5302a10458a509abcbb06f7f0b
NetKBs <abijose535@gmail.com>
2024-11-06 17:28:42 -0400

    feat(): fisrt commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/NetKBs/backend-reviewapp/config"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	config.LoadEnv()
+}
+
+func main() {
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
