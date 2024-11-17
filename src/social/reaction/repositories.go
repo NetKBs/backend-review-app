906361cc8c6eb0355d24631d878420cdec8c70cd
NetKBs <abijose535@gmail.com>
2024-11-17 16:27:58 -0400

    feat(src/review, src/image): create CRUD endpoints for review with image save/delete handler

--- src/social/reaction/repositories.go
+++ src/social/reaction/repositories.go
@@ -5,14 +5,14 @@ import (
 	"github.com/NetKBs/backend-reviewapp/src/schema"
 )
 
-func GetReactionsCountRepository(id uint) (reactionsCount map[string]uint, err error) {
+func GetReactionsCountRepository(id uint, contentType string) (reactionsCount map[string]uint, err error) {
 	db := config.DB
 	var likesCount, dislikesCount int64
 
-	if err = db.Model(&schema.Reaction{}).Where("content_id = ? AND content_type = 'review' AND reaction_type = true", id).Count(&likesCount).Error; err != nil {
+	if err = db.Model(&schema.Reaction{}).Where("content_id = ? AND content_type = ? AND reaction_type = true", id, contentType).Count(&likesCount).Error; err != nil {
 		return map[string]uint{"likes": 0, "dislikes": 0}, err
 	}
-	if err = db.Model(&schema.Reaction{}).Where("content_id = ? AND content_type = 'review' AND reaction_type = false", id).Count(&dislikesCount).Error; err != nil {
+	if err = db.Model(&schema.Reaction{}).Where("content_id = ? AND content_type = ? AND reaction_type = false", id, contentType).Count(&dislikesCount).Error; err != nil {
 		return map[string]uint{"likes": 0, "dislikes": 0}, err
 	}
 
--- src/social/review/services.go
+++ src/social/review/services.go
@@ -1,6 +1,8 @@
 package review
 
 import (
+	"github.com/NetKBs/backend-reviewapp/src/image"
+	"github.com/NetKBs/backend-reviewapp/src/schema"
 	"github.com/NetKBs/backend-reviewapp/src/social/comment"
 	"github.com/NetKBs/backend-reviewapp/src/social/reaction"
 )
@@ -12,12 +14,17 @@ func GetReviewByIdService(id uint) (reviewDTO ReviewResponseDTO, err error) {
 		return reviewDTO, err
 	}
 
-	reactionsCount, err := reaction.GetReactionsCountService(id)
+	reactionsCount, err := reaction.GetReactionsCountService(id, "review")
 	if err != nil {
 		return reviewDTO, err
 	}
 
 	commentsCount, err := comment.GetCommentsCountService(id)
+	if err != nil {
+		return reviewDTO, err
+	}
+
+	imagesPath, err := image.GetReviewImagesService(id)
 	if err != nil {
 		return reviewDTO, err
 	}
@@ -31,9 +38,36 @@ func GetReviewByIdService(id uint) (reviewDTO ReviewResponseDTO, err error) {
 		Likes:     reactionsCount["likes"],
 		Dislikes:  reactionsCount["dislikes"],
 		Comments:  commentsCount,
+		Images:    imagesPath,
 		CreatedAt: review.CreatedAt.String(),
 		UpdatedAt: review.UpdatedAt.String(),
 	}
 
 	return reviewDTO, nil
+}
+
+func CreateReviewService(review ReviewCreateDTO) (id uint, err error) {
+	reviewSchema := schema.Review{UserId: review.UserId, PlaceId: review.PlaceId, Text: review.Text, Rate: review.Rate}
+	id, err = CreateReviewRepository(reviewSchema)
+	if err != nil {
+		return id, err
+	}
+	return id, nil
+}
+
+func UpdateReviewService(id uint, review ReviewUpdateDTO) (err error) {
+	reviewSchema := schema.Review{Text: review.Text}
+	err = UpdateReviewRepository(id, reviewSchema)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func DeleteReviewService(id uint) (err error) {
+	err = DeleteReviewRepository(id)
+	if err != nil {
+		return err
+	}
+	return nil
 }
