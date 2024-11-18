ae2029040e3a9c8a54ce7825ddb7d50092680f71
Victor <vjxoen@gmail.com>
2024-11-18 12:51:33 -0400

    add user update repo

--- src/social/user/repositories.go
+++ src/social/user/repositories.go
@@ -14,3 +14,20 @@ func GetUserByIdRepository(id uint) (user schema.User, err error) {
 
 	return user, nil
 }
+
+func UpdateUserRepository(user *schema.User) error {
+	db := config.DB
+	tx := db.Begin()
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	err := tx.Save(user).Error
+	if err != nil {
+		tx.Rollback()
+	}
+	tx.Commit()
+	return err
+}
