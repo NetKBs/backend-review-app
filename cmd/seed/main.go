abe998514669bd1c99740605047d32e991b3086d
NetKBs <abijose535@gmail.com>
2024-12-15 00:00:47 -0400

    feat(cmd/seed): improve seeding
    Simplify code and more fake data

--- cmd/seed/main.go
+++ cmd/seed/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 
-	"github.com/NetKBs/backend-reviewapp/cmd/seed/fakeSchema"
 	"github.com/NetKBs/backend-reviewapp/config"
 	"github.com/NetKBs/backend-reviewapp/src/schema"
 	"github.com/brianvoe/gofakeit/v7"
@@ -19,106 +18,18 @@ func main() {
 	db := config.DB
 
 	if err := seedDatabase(db); err != nil {
-		log.Fatalf("Error al realizar el seeding: %v", err)
+		log.Fatalf("Error seeding database: %v", err)
 	} else {
 		log.Println("Database seeded successfully")
 	}
 
 }
 
-func seedDatabase(db *gorm.DB) (error error) {
+func seedDatabase(db *gorm.DB) error {
+	log.Println("Seeding database...")
 	gofakeit.Seed(777)
 
-	/*
-		usersId, error := seedUsers(db)
-		if error != nil {
-			return error
-		}
-
-			placesId, error := seedPlaces(db)
-			if error != nil {
-				return error
-			}
-
-			reviewsId, error := seedReviews(db, usersId, placesId)
-			if error != nil {
-				return error
-			}
-
-			commentsId, error := seedComments(db, usersId, reviewsId)
-			if error != nil {
-				return error
-			}
-
-			answersId, error := seedAnswers(db, usersId, commentsId)
-			if error != nil {
-				return error
-			}
-
-			error = seedReviewImages(db, reviewsId)
-			if error != nil {
-				return error
-			}
-
-			error = seedNotifications(db, usersId)
-			if error != nil {
-				return error
-			}
-
-			error = seedReactions(db, usersId, reviewsId, commentsId, answersId)
-			if error != nil {
-				return error
-			}
-
-			error = seedVisitedPlaces(db, usersId, placesId)
-			if error != nil {
-				return error
-			}
-
-			error = seedFollowersAndFollowings(db, usersId)
-			if error != nil {
-				return error
-			}
-
-			error = seedBookmarks(db, usersId, placesId)
-			if error != nil {
-				return error
-			}
-	*/
-	return nil
-}
-
-func seedUsers(db *gorm.DB) (usersId []uint, error error) {
-	avatarUrl := "https://picsum.photos/200/200"
-	userPassword, err := bcrypt.GenerateFromPassword([]byte("12345678"), bcrypt.DefaultCost)
-	if err != nil {
-		return usersId, err
-	}
-
-	for i := 0; i < 10; i++ {
-		userFake := fakeSchema.UserFaker{}
-		if err := gofakeit.Struct(&userFake); err != nil {
-			return usersId, err
-		}
-
-		user := schema.User{
-			Username:    userFake.Username,
-			AvatarUrl:   &avatarUrl,
-			DisplayName: userFake.DisplayName,
-			Email:       userFake.Email,
-			Password:    string(userPassword),
-		}
-
-		if err := db.Create(&user).Error; err != nil {
-			return usersId, err
-		}
-		usersId = append(usersId, user.ID)
-	}
-
-	return usersId, nil
-}
-
-func seedPlaces(db *gorm.DB) (placesId []uint, error error) {
+	// Places
 	mapsId := [10]string{
 		"512f8a79b18b5e4fc059481220a886962040f00103f9013ebcd3f501000000920318415354524f4d204c6962726572c3ad61202620436166c3a9",
 		"5141b62c5f975e4fc059e87472da75962040f00103f9013fbcd3f50100000092030a4d61786920446f6e6173",
@@ -131,247 +42,211 @@ func seedPlaces(db *gorm.DB) (placesId []uint, error error) {
 		"5136a8a21ddc5e4fc059375cc4cbf5952040f00103f9011d5d52e00100000092030fc3937074696361204361726f6ec3ad",
 		"51f34d3f4dd25e4fc0594863abc403962040f00103f9012894c390010000009203094d657263616e74696c",
 	}
-
+	var places []schema.Place
 	for _, mapsId := range mapsId {
 		place := schema.Place{
 			MapsId: mapsId,
 		}
-		if err := db.Create(&place).Error; err != nil {
-			return placesId, err
-		}
-		placesId = append(placesId, place.ID)
+		places = append(places, place)
+	}
+	if err := db.Create(&places).Error; err != nil {
+		return err
+	}
+	log.Println("Places seeded successfully")
+
+	// User
+	avatarUrl := "https://picsum.photos/200/200"
+	userPassword, err := bcrypt.GenerateFromPassword([]byte("12345678"), bcrypt.DefaultCost)
+	if err != nil {
+		return err
 	}
 
-	return placesId, nil
-}
-
-/*
-func seedReviews(db *gorm.DB, placesId []uint, usersId []uint) (reviewsId []uint, error error) {
-
+	var users []schema.User
 	for i := 0; i < 30; i++ {
-		reviewFake := fakeSchema.ReviewFaker{}
-		if err := gofakeit.Struct(&reviewFake); err != nil {
-			return reviewsId, err
+		user := schema.User{
+			Username:    gofakeit.Username(),
+			AvatarUrl:   &avatarUrl,
+			DisplayName: gofakeit.Name(),
+			Email:       gofakeit.Email(),
+			Password:    string(userPassword),
 		}
-
-		review := schema.Review{
-			UserId:  usersId[i%len(usersId)],
-			PlaceId: placesId[i%len(placesId)],
-			Rate:    reviewFake.Rate,
-			Text:    reviewFake.Text,
-		}
-		if err := db.Create(&review).Error; err != nil {
-			return reviewsId, err
-		}
-		reviewsId = append(reviewsId, review.ID)
+		users = append(users, user)
 	}
+	if err := db.Create(&users).Error; err != nil {
+		return err
+	}
+	log.Println("Users seeded successfully")
 
-	return reviewsId, nil
-}
-
-func seedReviewImages(db *gorm.DB, reviewsId []uint) (err error) {
-	for _, reviewId := range reviewsId {
-		reviewImages := []schema.ReviewImage{
-			{ReviewId: reviewId, ImageURL: "https://picsum.photos/600/400"},
-			{ReviewId: reviewId, ImageURL: "https://picsum.photos/600/400"},
-		}
-		if err := db.Create(&reviewImages).Error; err != nil {
-			return err
+	// Reviews
+	var reviews []schema.Review
+	for i := 0; i < len(places); i++ {
+		for j := 0; j < len(users); j++ {
+			review := schema.Review{
+				UserId:  users[j].ID,
+				PlaceId: places[i].ID,
+				Rate:    uint(gofakeit.Number(1, 5)),
+				Text:    gofakeit.Sentence(gofakeit.Number(20, 50)),
+			}
+			reviews = append(reviews, review)
 		}
 	}
-	return nil
-}
-
-func seedComments(db *gorm.DB, usersId []uint, reviewsId []uint) (commentsId []uint, err error) {
-
-	for i := 0; i < 30; i++ {
-		commentFake := fakeSchema.CommentFaker{}
-		if err := gofakeit.Struct(&commentFake); err != nil {
-			return commentsId, err
-		}
-
-		comment := schema.Comment{
-			UserId:   usersId[i%len(usersId)],
-			ReviewId: reviewsId[i%len(reviewsId)],
-			Text:     commentFake.Text,
-		}
-
-		if err := db.Create(&comment).Error; err != nil {
-			return commentsId, err
-		}
-		commentsId = append(commentsId, comment.ID)
+	if err := db.Create(&reviews).Error; err != nil {
+		return err
 	}
+	log.Println("Reviews seeded successfully")
 
-	return commentsId, nil
-}
-
-func seedAnswers(db *gorm.DB, usersId []uint, commentsId []uint) (answersId []uint, err error) {
-
-	for i := 0; i < 30; i++ {
-		answerFake := fakeSchema.AnswerFaker{}
-		if err := gofakeit.Struct(&answerFake); err != nil {
-			return answersId, err
-		}
-
-		answer := schema.Answer{
-			UserId:    usersId[i%len(usersId)],
-			CommentId: commentsId[i%len(commentsId)],
-			Text:      answerFake.Text,
-		}
-		if err := db.Create(&answer).Error; err != nil {
-			return answersId, err
-		}
-		answersId = append(answersId, answer.ID)
-	}
-
-	return answersId, nil
-}
-
-func seedNotifications(db *gorm.DB, usersId []uint) (err error) {
-
-	for _, userId := range usersId {
-		notificationFake := fakeSchema.NotificationFaker{}
-		if err := gofakeit.Struct(&notificationFake); err != nil {
-			return err
-		}
-
-		notification := schema.Notification{
-			UserId: userId,
-			Text:   notificationFake.Text,
-		}
-
-		if err := db.Create(&notification).Error; err != nil {
-			return err
+	// Reviews Images
+	var reviewImages []schema.ReviewImage
+	for i := 0; i < len(reviews); i++ {
+		for j := 0; j < 3; j++ {
+			reviewImage := schema.ReviewImage{
+				ReviewId: reviews[i].ID,
+				ImageURL: "https://picsum.photos/600/400",
+			}
+			reviewImages = append(reviewImages, reviewImage)
 		}
 	}
+	if err := db.Create(&reviewImages).Error; err != nil {
+		return err
+	}
+	log.Println("Reviews Images seeded successfully")
 
-	return nil
-}
-
-func seedReactions(db *gorm.DB, usersId []uint, reviewsId []uint, commentsId []uint, answersId []uint) (err error) {
-	for i, reviewId := range reviewsId {
-		reactionFake := fakeSchema.ReactionFaker{}
-		if err := gofakeit.Struct(&reactionFake); err != nil {
-			return err
+	// Comments
+	var comments []schema.Comment
+	for i := 0; i < len(reviews); i += 10 {
+		for j := 0; j < len(users); j++ {
+			comment := schema.Comment{
+				UserId:   users[j].ID,
+				ReviewId: reviews[i].ID,
+				Text:     gofakeit.Sentence(gofakeit.Number(20, 50)),
+			}
+			comments = append(comments, comment)
 		}
+	}
+	if err := db.Create(&comments).Error; err != nil {
+		return err
+	}
+	log.Println("Comments seeded successfully")
 
+	// Answers
+	var answers []schema.Answer
+	for i := 0; i < len(comments); i += 30 {
+		for j := 0; j < len(users); j++ {
+			answer := schema.Answer{
+				UserId:    users[j].ID,
+				CommentId: comments[i].ID,
+				Text:      gofakeit.Sentence(gofakeit.Number(20, 50)),
+			}
+			answers = append(answers, answer)
+		}
+	}
+	if err := db.Create(&answers).Error; err != nil {
+		return err
+	}
+	log.Println("Answers seeded successfully")
+
+	// Reactions for Reviews, Comments and Answers
+	var reactions []schema.Reaction
+
+	for i, review := range reviews {
 		reaction := schema.Reaction{
-			UserId:       usersId[i%len(usersId)],
-			ContentId:    reviewId,
+			UserId:       users[i%len(users)].ID,
+			ContentId:    review.ID,
 			ContentType:  "review",
-			ReactionType: reactionFake.ReactionType,
+			ReactionType: gofakeit.Bool(),
 		}
-
-		if err := db.Create(&reaction).Error; err != nil {
-			return err
-		}
+		reactions = append(reactions, reaction)
 	}
 
-	for i, commentId := range commentsId {
-		reactionFake := fakeSchema.ReactionFaker{}
-		if err := gofakeit.Struct(&reactionFake); err != nil {
-			return err
-		}
-
+	for i := 0; i < len(comments); i += 10 {
 		reaction := schema.Reaction{
-			UserId:       usersId[i%len(usersId)],
-			ContentId:    commentId,
+			UserId:       users[i%len(users)].ID,
+			ContentId:    comments[i].ID,
 			ContentType:  "comment",
-			ReactionType: reactionFake.ReactionType,
+			ReactionType: gofakeit.Bool(),
 		}
-
-		if err := db.Create(&reaction).Error; err != nil {
-			return err
-		}
+		reactions = append(reactions, reaction)
 	}
 
-	for i, answerId := range answersId {
-		reactionFake := fakeSchema.ReactionFaker{}
-		if err := gofakeit.Struct(&reactionFake); err != nil {
-			return err
-		}
-
+	for i := 0; i < len(answers); i += 10 {
 		reaction := schema.Reaction{
-			UserId:       usersId[i%len(usersId)],
-			ContentId:    answerId,
+			UserId:       users[i%len(users)].ID,
+			ContentId:    answers[i].ID,
 			ContentType:  "answer",
-			ReactionType: reactionFake.ReactionType,
+			ReactionType: gofakeit.Bool(),
 		}
-
-		if err := db.Create(&reaction).Error; err != nil {
-			return err
-		}
+		reactions = append(reactions, reaction)
 	}
 
-	return nil
-}
-
-func seedVisitedPlaces(db *gorm.DB, usersId []uint, placesId []uint) (err error) {
-	for _, userId := range usersId {
-		for i := 0; i < gofakeit.Number(1, len(placesId))-1; i++ {
-
-			var place schema.Place
-			if err := db.Find(&place, placesId[i]).Error; err != nil {
-				return err
-			}
-			var user schema.User
-			if err := db.Find(&user, userId).Error; err != nil {
-				return err
-			}
-
-			if err := db.Model(&user).Association("VisitedPlaces").Append(&place); err != nil {
-				return err
-			}
-		}
+	if err := db.Create(&reactions).Error; err != nil {
+		return err
 	}
+	log.Println("Reactions successfully seeded")
 
-	return nil
-}
+	// Visited places
+	for _, user := range users {
+		for i := 0; i < len(places); i += 2 {
 
-func seedFollowersAndFollowings(db *gorm.DB, usersId []uint) (err error) {
-	for i := 0; i < len(usersId); i++ {
-		var user1 schema.User
-		if err := db.Find(&user1, usersId[i]).Error; err != nil {
-			return err
-		}
-
-		for j := 0; j < gofakeit.Number(0, len(usersId)-1); j++ {
-			var user2 schema.User
-			if err := db.Find(&user2, usersId[j]).Error; err != nil {
+			var place_model schema.Place
+			if err := db.Find(&place_model, places[i].ID).Error; err != nil {
+				return err
+			}
+			var user_model schema.User
+			if err := db.Find(&user_model, user.ID).Error; err != nil {
 				return err
 			}
 
-			if err := db.Model(&user1).Association("Followers").Append(&user2); err != nil {
-				return err
-			}
-			if err := db.Model(&user1).Association("Following").Append(&user2); err != nil {
+			if err := db.Model(&user_model).Association("VisitedPlaces").Append(&place_model); err != nil {
 				return err
 			}
 		}
 	}
+	log.Println("Visited places seeded successfully")
 
-	return nil
-}
-
-func seedBookmarks(db *gorm.DB, usersId []uint, placesId []uint) (err error) {
-	for i := 0; i < len(usersId); i++ {
-		var user schema.User
-		if err := db.Find(&user, usersId[i]).Error; err != nil {
-			return err
-		}
-
-		for j := 0; j < gofakeit.Number(0, len(placesId)-1); j++ {
-			var place schema.Place
-			if err := db.Find(&place, placesId[j]).Error; err != nil {
+	// Bookmark
+	for _, user := range users {
+		for i := 0; i < len(places); i += 2 {
+			var place_model schema.Place
+			if err := db.Find(&place_model, places[i].ID).Error; err != nil {
+				return err
+			}
+			var user_model schema.User
+			if err := db.Find(&user_model, user.ID).Error; err != nil {
 				return err
 			}
 
-			if err := db.Model(&user).Association("BookmarkedPlaces").Append(&place); err != nil {
+			if err := db.Model(&user_model).Association("BookmarkedPlaces").Append(&place_model); err != nil {
+				return err
+			}
+		}
+	}
+	log.Println("Bookmarks seeded successfully")
+
+	// Followers and followings
+	for _, user := range users {
+		for i := 0; i < len(users); i += 2 {
+			if users[i].ID == user.ID {
+				continue
+			}
+			var user_model schema.User
+			if err := db.Find(&user_model, user.ID).Error; err != nil {
+				return err
+			}
+			var user_model2 schema.User
+			if err := db.Find(&user_model2, users[i].ID).Error; err != nil {
+				return err
+			}
+
+			if err := db.Model(&user_model).Association("Followers").Append(&user_model2); err != nil {
+				return err
+			}
+			if err := db.Model(&user_model2).Association("Following").Append(&user_model); err != nil {
 				return err
 			}
 		}
 	}
 
+	log.Println("Followers and followings seeded successfully")
+
 	return nil
 }
-*/
