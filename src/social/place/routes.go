04c1708663ac2e1edf96b1fff24ab66de7e4447b
NetKBs <abijose535@gmail.com>
2024-12-20 13:55:19 -0400

    feat(): add auth middleware to routes

--- src/social/place/routes.go
+++ src/social/place/routes.go
@@ -1,11 +1,12 @@
 package place
 
 import (
+	"github.com/NetKBs/backend-reviewapp/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
-	group := router.Group("/places")
+	group := router.Group("/places", middlewares.AuthMiddleware())
 
 	group.GET("/", getPlaceController)
 }
--- src/social/user/routes.go
+++ src/social/user/routes.go
@@ -1,20 +1,23 @@
 package user
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/NetKBs/backend-reviewapp/src/middlewares"
+	"github.com/gin-gonic/gin"
+)
 
 func RegisterRoutes(router *gin.Engine) {
 	users := router.Group("/users")
 	{
-		users.GET("/verify/:username", UserExistsByUsernameController)
-		users.GET("/:id", GetUserByIdController)
+		users.GET("/verify/:username", UserExistsByUsernameController) // checks
+		users.GET("/:id", middlewares.AuthMiddleware(), GetUserByIdController)
 
-		users.POST("/", CreateUserController)
-		users.DELETE("/:id", DeleteUserbyIdController)
+		users.POST("/", CreateUserController) // register
+		users.DELETE("/:id", middlewares.AuthMiddleware(), DeleteUserbyIdController)
 
-		users.PUT("/displayname/:id", UpdateUserDisplayNameController)
-		users.PUT("/username/:id", UpdateUsernameUserController)
-		users.PUT("/password/:id", UpdatePasswordUserController)
-		users.PUT("/avatar/:id", UpdateAvatarUserController)
-		users.PUT("/email/:id", UpdateEmailUserController)
+		users.PUT("/displayname/:id", middlewares.AuthMiddleware(), UpdateUserDisplayNameController)
+		users.PUT("/username/:id", middlewares.AuthMiddleware(), UpdateUsernameUserController)
+		users.PUT("/password/:id", middlewares.AuthMiddleware(), UpdatePasswordUserController)
+		users.PUT("/avatar/:id", middlewares.AuthMiddleware(), UpdateAvatarUserController)
+		users.PUT("/email/:id", middlewares.AuthMiddleware(), UpdateEmailUserController)
 	}
 }
