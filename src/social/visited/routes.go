33ccf1fa76cb957e5f81155b2affced560bdb10a
Victor <vjxoen@gmail.com>
2025-02-12 20:56:36 -0400

    mini commit

--- src/social/visited/routes.go
+++ src/social/visited/routes.go
@@ -1,12 +1,12 @@
 package visited
 
 import (
-	//"github.com/NetKBs/backend-reviewapp/src/middlewares"
+	"github.com/NetKBs/backend-reviewapp/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterRoutes(router *gin.Engine) {
-	visited := router.Group("/visited" /* middlewares.AuthMiddleware()*/)
+	visited := router.Group("/visited", middlewares.AuthMiddleware())
 	{
 		visited.GET("/count/:user_id", GetVisitedCountController)
 		visited.GET("/visitors/:place_id", GetVisitorsCount)
