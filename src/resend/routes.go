2fb6d30e02171bf94d5bf20cb46d9bd6c51e49a0
NetKBs <abijose535@gmail.com>
2025-02-20 16:20:45 -0400

    feat(src/middlewares, src/resend): create requests limit middleware and apply to resend route

--- src/resend/routes.go
+++ src/resend/routes.go
@@ -1,6 +1,8 @@
 package resend
 
 import (
+	"time"
+
 	"github.com/NetKBs/backend-reviewapp/src/middlewares"
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +11,7 @@ func RegisterRoutes(router *gin.Engine) {
 	resend := router.Group("/code", middlewares.AuthMiddleware())
 	{
 
-		resend.POST("/generate", generateVerificationCodeController)
+		resend.POST("/generate", middlewares.RateLimitMiddleware(1, 30*time.Second), generateVerificationCodeController)
 		resend.POST("/verify", verifyVerificationCodeController)
 
 	}
