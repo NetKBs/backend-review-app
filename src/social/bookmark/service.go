a28d83763635d2759dd4512c7e7ec82cb1398501
Andres1555 <98991190+Andres1555@users.noreply.github.com>
2025-02-24 03:28:19 -0400

    Update service.go

--- src/social/bookmark/service.go
+++ src/social/bookmark/service.go
@@ -1,9 +1,35 @@
 package bookmark
 
-func GetBookmarkCount(userId uint) (bookmarkCount uint, err error) {
-	bookmarkCount, err = GetBookmarkCountRepository(userId)
+import (
+	"errors"
+)
+
+func GetBookmarks(userId uint) ([]uint, error) {
+	bookmarkedPlaces, err := GetBookmarksRepository(userId)
 	if err != nil {
-		return bookmarkCount, err
+		return nil, err
 	}
-	return bookmarkCount, nil
+
+	var placeIDs []uint
+	for _, place := range bookmarkedPlaces {
+		placeIDs = append(placeIDs, place.ID)
+	}
+
+	return placeIDs, nil
+}
+
+func bookmarkpostservice(userId uint, placeID uint) error {
+	err := bookmarkpostrepository(userId, placeID)
+	if err != nil {
+		return errors.New("error al marcar el lugar como favorito")
+	}
+	return nil
+}
+
+func bookmarkdeleteservice(userID uint, placeID uint) error {
+	err := bookmarkdeleterepository(userID, placeID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
